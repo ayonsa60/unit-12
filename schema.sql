9a693b74b3b4050df5b5e001060e1a67c22862ec
ayonsa60 <ayonsa60@gmail.com>
2021-12-07 16:33:12 -0600

    completed schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,24 @@
+DROP DATABASE IF EXIST
 CREATE DATABASE employees;
 
 USE employees;
 
 CREATE TABLE department (
-    
-)
+    id INT,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    INDEX dep_ind (department_id),
+);
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+);
